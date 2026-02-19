66f5df2363d333663b34087f839022e82567b97d
rchowd9 <135185247+rchowd9@users.noreply.github.com>
2026-02-19 16:05:58 -0500

    Make radius field private in Circle class
    Changed radius field from public to private for encapsulation.

--- java/src/main/java/edu/oldwestbury/se/lab/shapes/Circle.java
+++ java/src/main/java/edu/oldwestbury/se/lab/shapes/Circle.java
@@ -1,7 +1,7 @@
 package edu.oldwestbury.se.lab.shapes;
 
 public class Circle implements Shape {
-    public double radius;
+    private double radius;
 
     public Circle(double radius) {
         this.radius = radius;
