5215d5e650ee52788af3505cd2d85236d9695b72
rchowd9 <135185247+rchowd9@users.noreply.github.com>
2026-02-19 15:58:36 -0500

    Update Rectangle.java

--- java/src/main/java/edu/oldwestbury/se/lab/shapes/Rectangle.java
+++ java/src/main/java/edu/oldwestbury/se/lab/shapes/Rectangle.java
@@ -1,8 +1,8 @@
 package edu.oldwestbury.se.lab.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
+    private double height;
+    private double width;
     
     public Rectangle(double height, double width){
         this.height = height;
